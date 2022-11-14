9bb349994008ad680c8e8a8ada465bd2fdb5b163
duban <tosha1600@mail.ru>
2022-11-14 22:27:07 +0300

    deleted id from subcategory

--- OnlineStore.Domain/Models/Subcategory.cs
+++ OnlineStore.Domain/Models/Subcategory.cs
@@ -5,7 +5,6 @@ namespace OnlineStore.Domain.Models
     [BsonCollectionAttribute("SubCategory")]
     public class Subcategory : Model
     {
-        public string Id { get; set; }
         public string Name { get; set; }
         public Category Category { get; set; }
     }
