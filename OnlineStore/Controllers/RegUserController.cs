ece40ea99bcc87f871e6c9683877d02fe1477698
duban <tosha1600@mail.ru>
2022-11-17 01:18:50 +0300

    changed result of get method

--- OnlineStore/Controllers/RegUserController.cs
+++ OnlineStore/Controllers/RegUserController.cs
@@ -14,18 +14,22 @@ namespace OnlineStore.Controllers
         {
             _productService = productService;
         }
+
         [HttpGet]
         [Route("/products")]
         public async Task<ActionResult<List<Product>>> Get()
         {
            var basket = await _productService.GetProducts(p=>p.Subcategory.Category.Name == "cat1");
-           return basket == null ? NotFound() : basket;
+        
+            return basket == null ? NotFound() : Ok(basket);
         }
+
         [HttpGet]
         [Route("/products/{id}")]
         public async Task<FileResult> GetImage(string id)
         {
             Product p = await _productService.GetProduct(id);
+        
             return new FileStreamResult(System.IO.File.OpenRead(p.Image), "image/png");
         }
     }
