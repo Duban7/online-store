7084632950d16f205dc99ad3fc8c3a9b596598c6
duban <tosha1600@mail.ru>
2022-11-19 06:49:58 +0300

    Revert "git is mad on me, so i need to change that shit"
    This reverts commit b2e8c383b1fc75c83036b2b183994a6ea5491da7.

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -1,42 +1,11 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using OnlineStore.BLL.AccountService;
-using OnlineStore.DAL;
-using OnlineStore.Domain.Models;
-using OnlineStore.Options;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = JwtOptions.ISSUER,
-        ValidateAudience = true,
-        ValidAudience = JwtOptions.AUDIENCE,
-        ValidateLifetime = true,
-        IssuerSigningKey = JwtOptions.GetSymmetricSecurityKey(),
-        ValidateIssuerSigningKey = true,
-    };
-});
-builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("OnlineStoreDatabase"));
-builder.Services.AddSingleton<IRepository<User>,MongoDBRepository<User>>();
-builder.Services.AddSingleton<IRepository<RegUser>, MongoDBRepository<RegUser>>();
-builder.Services.AddSingleton<AccountService>();
+builder.Services.Configure<OnlineStore.DAL.DatabaseSettings>(builder.Configuration.GetSection("OnlineStoreDatabase"));
+builder.Services.AddSingleton<OnlineStore.DAL.MongoDBRepository<OnlineStore.Domain.Models.Basket>>();
 builder.Services.AddControllers();
-
 var app = builder.Build();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
 app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
 app.UseEndpoints (endpoints =>
 {
     endpoints.MapControllers();
 });
-
 app.Run();
