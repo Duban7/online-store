ef5c1232153fadaf2f84e05380fc6c24885a6c55
duban <tosha1600@mail.ru>
2022-11-19 06:44:18 +0300

    added expression class to Repository predicate method

--- OnluneStore.DAL/IRepository.cs
+++ OnluneStore.DAL/IRepository.cs
@@ -1,12 +1,14 @@
-﻿namespace OnlineStore.DAL
+﻿using System.Linq.Expressions;
+
+namespace OnlineStore.DAL
 {
     public interface IRepository<T> where T : class
     {
         public Task<List<T>> GetAsync();
-        public Task<List<T>?> GetAsync(Func<T, bool> predicate);
+        public Task<List<T>?> GetAsync(Expression<Func<T, bool>> predicate);
         public Task<T?> GetAsync(string id);
-        public Task CreateAsync(T newBasket);
-        public Task UpdateAsync(string id, T updatedBasket);
+        public Task CreateAsync(T newModel);
+        public Task UpdateAsync(string id, T updatedModel);
         public Task RemoveAsync(string id);
     }
 }
--- OnluneStore.DAL/MongoDBRepository.cs
+++ OnluneStore.DAL/MongoDBRepository.cs
@@ -1,21 +1,26 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using OnlineStore.Domain.CustomAttribute;
 using OnlineStore.Domain.Models;
+using System.Linq.Expressions;
 
 namespace OnlineStore.DAL
 {
     public class MongoDBRepository <TModel> : IRepository<TModel> where TModel : Model
     {
         private readonly IMongoCollection<TModel> _TModelCollection;
+        private readonly ILogger _logger;
 
-        public MongoDBRepository(IOptions<DatabaseSettings> OnlineStoreDataBaseSettings)
+        public MongoDBRepository(IOptions<DatabaseSettings> OnlineStoreDataBaseSettings, ILogger<MongoDBRepository<TModel>> logger)
         {
             MongoClient mongoClient = new MongoClient(OnlineStoreDataBaseSettings.Value.ConnectionString);
 
             IMongoDatabase mongoDataBase = mongoClient.GetDatabase(OnlineStoreDataBaseSettings.Value.DatabaseName);
 
             _TModelCollection = mongoDataBase.GetCollection<TModel>(GetCollectionName(typeof(TModel)));
+            _logger = logger;
         }
         private protected string GetCollectionName(Type documentType)
         {
@@ -23,8 +28,10 @@ namespace OnlineStore.DAL
         }
         public async Task<List<TModel>> GetAsync() =>
             await _TModelCollection.Find(_ => true).ToListAsync<TModel>();
-        public async Task<List<TModel>?> GetAsync(Func<TModel,bool> predicant) =>
-            await _TModelCollection.Find(_ => true).ToListAsync<TModel>();
+
+        public async Task<List<TModel>?> GetAsync(Expression<Func<TModel, bool>> predicant) =>
+             await _TModelCollection.Find(predicant).ToListAsync<TModel>();
+          
         public async Task<TModel?> GetAsync(string id) =>
             await _TModelCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
@@ -36,5 +43,8 @@ namespace OnlineStore.DAL
 
         public async Task RemoveAsync(string id) =>
             await _TModelCollection.DeleteOneAsync(x => x.Id == id);
+
+        public string GenerateID() =>
+            ObjectId.GenerateNewId().ToString();
     }
 }
