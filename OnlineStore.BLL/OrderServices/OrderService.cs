879690d3eaf3622957dd5e32d2347fd49c4a27a8
matthewtorro <matik27004@gmail.com>
2022-11-20 03:30:08 +0300

    added new endpoints

---
+++ OnlineStore.BLL/OrderServices/OrderService.cs
@@ -0,0 +1,54 @@
+﻿using OnlineStore.Domain.Models;
+using OnlineStore.DAL;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
+using System.Security.Principal;
+
+namespace OnlineStore.BLL.OrderServices
+{
+    public class OrderService
+    {
+        private readonly IRepository<Order> _orderrepository;
+        private readonly IRepository<Basket> _basketrepository;
+        private readonly ILogger<OrderService> _logger;
+
+        public OrderService(IRepository<Order> repository, IRepository<Basket> basketrepository, ILogger<OrderService> logger)
+        {
+            _orderrepository = repository;
+            _basketrepository = basketrepository;
+            _logger = logger;
+        }
+
+        public async Task<List<Order>> GetOrders(Expression<Func<Order, bool>> predicante)
+        {
+            return await _orderrepository.GetAsync(predicante);
+        }
+
+        public async Task<Basket> GetBasket(string id)
+        {
+            return await _basketrepository.GetAsync(id);
+        }
+        public async Task<List<Basket>> GetBasket(Expression<Func<Basket, bool>> predicante)
+        {
+            return await _basketrepository.GetAsync(predicante);
+        }
+
+        public async Task<Order> CreateOrder(Basket newBasket)
+        {
+            DateTime timeNow = new DateTime();
+            string id = (_orderrepository as MongoDBRepository<Order>).GenerateID();
+            Order newOrder = new Order()
+            {
+                Id = id,
+                Date = timeNow,
+                IdUser = newBasket.IdUser,
+                Products = newBasket.Products,
+                TotalSum = newBasket.TotalSum
+            };
+            await _orderrepository.CreateAsync(newOrder);
+            return newOrder;
+        }
+        
+    }
+}
---
+++ OnlineStore/Controllers/OrdersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.BLL.OrderServices;
+using OnlineStore.Domain.Models;
+using System.Security.Principal;
+using static MongoDB.Driver.WriteConcern;
+
+namespace OnlineStore.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class OrdersController : ControllerBase
+    {
+        OrderService _orderService;
+        public OrdersController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+ 
+        [HttpGet]
+        [Route("/clients/{IdUser}/orders")]
+        public async Task<ActionResult<List<Order>>> Get(string IdUser)
+        {
+            var orders = await _orderService.GetBasket(p => p.IdUser == IdUser);
+
+            return orders == null ? NotFound() : Ok(orders);
+        }
+        
+        [HttpGet]
+        [Route("/clients/{IdUser}/basket")]
+        public async Task<ActionResult<List<Basket>>> GetUserBasket(string IdUser)
+        {
+            var basket  = await _orderService.GetBasket(p => p.IdUser == IdUser);
+
+            return basket == null ? NotFound() : Ok(basket);
+        }
+        
+
+        [HttpPost]
+        [Route("clients/{IdUser}/basket")]
+       public async Task<ActionResult<Order>> CreateNewOrder([FromBody] Basket newOrder)
+        {
+            Order order = await _orderService.CreateOrder(newOrder);
+
+            return order == null ? BadRequest() : Ok(order);
+        }
+        
+        
+    }
+}
